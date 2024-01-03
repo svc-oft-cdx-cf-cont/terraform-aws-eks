c6e97cdfd38ba2d9f091d112acb0b12f82bc2fca
o-grigorev <oleg_grigorev@mckinsey.com>
2024-01-03 11:09:58 +0100

    fix: WIZ rule complience EKS-011

--- variables.tf
+++ variables.tf
@@ -71,7 +71,6 @@ variable "cluster_endpoint_public_access" {
 variable "cluster_endpoint_public_access_cidrs" {
   description = "List of CIDR blocks which can access the Amazon EKS public API server endpoint"
   type        = list(string)
-  default     = ["0.0.0.0/0"]
 }
 
 variable "cluster_ip_family" {
